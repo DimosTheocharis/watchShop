e31d05c242f73b5e6750a858019a8e9c965b0c5a
Dimos Theocharis <dimostheocharis10@gmail.com>
2025-05-31 20:39:34 +0300

    Setup translation

---
+++ static/lang/en.js
@@ -0,0 +1,10 @@
+
+export const enTranslations = {
+    "Home": "Home",
+    "Products": "Products",
+    "Greek": "Greek (el)",
+    "English": "English (en)",
+    "HeroTitle": "Elevate Your Time",
+    "HeroText": "Discover meticulously crafted timepieces that blend precision, heritage, and contemporary style.",
+    "HeroButton": "Explore Collection"
+}
